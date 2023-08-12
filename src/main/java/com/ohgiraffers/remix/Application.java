ef491be484db1a0a558e8136b13b9b69155f3cfc
jooseohyeon <wntjguswkd@gmail.com>
2023-08-12 15:58:15 +0900

    test commit

--- src/main/java/com/ohgiraffers/remix/Application.java
+++ src/main/java/com/ohgiraffers/remix/Application.java
@@ -22,7 +22,7 @@ public class Application {
 
             switch (no){
                 case 1 : travelController.selectAllMenu(); break;
-                case 2 : break;
+                case 2 : travelController.break;
                 case 3 : break;
                 case 4 : break;
                 case 5 : break;
